264387746605b76f7f922163b109ef7da80c3f59
devGO <been2zz@naver.com>
2024-11-14 21:02:34 +0900

    feat: api config

---
+++ src/config/apiConfig.ts
@@ -0,0 +1,5 @@
+export const API_BASE_URL = 'http://localhost:8080';
+export const API_ROUTES = {
+    USER_CREATE: `${API_BASE_URL}/users/create`,
+    USER_LOGIN: `${API_BASE_URL}/users/login`,
+};
