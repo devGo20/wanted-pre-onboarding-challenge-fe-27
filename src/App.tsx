3129da4e87a7e47b6783b20e2603dcef81ce1dd5
devGO <been2zz@naver.com>
2024-11-15 14:19:34 +0900

    feat: Tostify add

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Auth/Login';
 import Signup from './pages/Auth/Signup';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/auth/login" element={<Login />} />
-      <Route path="/auth/signup" element={<Signup />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/signup" element={<Signup />} />
+      </Routes>
+      <ToastContainer
+        position="bottom-center"
+        hideProgressBar={false} />
+    </>
   );
 }
 
