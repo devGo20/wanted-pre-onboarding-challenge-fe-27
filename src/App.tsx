46b5c50770f2738a72bca05190fe08c3cb991acb
devGO <been2zz@naver.com>
2024-11-18 22:10:58 +0900

    feat: TodoList, TodoDetail router

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Login from './pages/Auth/Login';
 import Signup from './pages/Auth/Signup';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import TodoList from './pages/Todo/TodoList';
+import TodoDetail from './pages/Todo/TodoDetail';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/signup" element={<Signup />} />
+        <Route path="/todos" element={<TodoList />} />
+        <Route path="/todos/:id" element={<TodoDetail />} />
       </Routes>
       <ToastContainer
         position="bottom-center"
