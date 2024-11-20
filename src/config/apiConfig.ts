d6f9de8a049fc5bf28ebc280ddfec00fed97db51
devGO <been2zz@naver.com>
2024-11-20 21:37:37 +0900

    fix: apiConfig

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,5 +1,6 @@
 export const API_BASE_URL = 'http://localhost:8080';
 export const API_ROUTES = {
-    USER_CREATE: `${API_BASE_URL}/users/create`,
-    USER_LOGIN: `${API_BASE_URL}/users/login`,
+  USER_CREATE: `${API_BASE_URL}/users/create`,
+  USER_LOGIN: `${API_BASE_URL}/users/login`,
+  TODO: `${API_BASE_URL}/todos`,
 };
