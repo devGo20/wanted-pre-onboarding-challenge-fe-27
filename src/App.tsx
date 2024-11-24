379e77817b17c57cff4e386b582b827f09524697
devGO <been2zz@naver.com>
2024-11-24 21:59:28 +0900

    feat: QueryClient 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,29 @@ import Signup from './pages/Auth/Signup';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import TodoPage from './pages/Todo/TodoPage';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 2,
+    },
+  },
+});
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/auth/login" element={<Login />} />
-        <Route path="/auth/signup" element={<Signup />} />
-        <Route path="/todos/*" element={<TodoPage />} />
-      </Routes>
-      <ToastContainer
-        position="bottom-center"
-        hideProgressBar={false} />
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/auth/signup" element={<Signup />} />
+          <Route path="/todos/*" element={<TodoPage />} />
+        </Routes>
+        <ToastContainer
+          position="bottom-center"
+          hideProgressBar={false} />
+      </QueryClientProvider>
     </>
   );
 }
