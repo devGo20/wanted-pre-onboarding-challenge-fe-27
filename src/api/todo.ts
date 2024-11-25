8014862571f7de24a3ebe33692ea7edd1434d3a7
devGO <been2zz@naver.com>
2024-11-25 15:20:25 +0900

    fix: space 조정

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -2,49 +2,51 @@ import axios from "axios";
 import { API_ROUTES } from "../config/apiConfig";
 
 export const getTodos = async (token: string) => {
-    const response = await axios.get(API_ROUTES.TODO, {
-        headers: {
-            Authorization: token,
-        },
-    });
-    return response.data.data;
+	const response = await axios.get(API_ROUTES.TODO, {
+		headers: {
+			Authorization: token,
+		},
+	});
+
+	return response.data.data;
 };
 
 
 export const addTodo = async (title: string, content: string, token: string,) => {
-    const response = await axios.post(
-        API_ROUTES.TODO,
-        {
-            title: title,
-            content: content,
-        },
-        {
-            headers: {
-                Authorization: token,
-            },
-        }
-    );
-    return response.data.data;
+	const response = await axios.post(
+		API_ROUTES.TODO,
+		{
+			title,
+			content,
+		},
+		{
+			headers: {
+				Authorization: token,
+			},
+		}
+	);
+
+	return response.data.data;
 };
 
 
 export const updateTodo = async (id: string, title: string, content: string, token: string,) => {
-    const response = await axios.put(`${API_ROUTES.TODO}/${id}`, {
-        title: title,
-        content: content,
-    }, {
-        headers: {
-            Authorization: token,
-        },
-    });
-    return response.data.data;
+	const response = await axios.put(`${API_ROUTES.TODO}/${id}`, {
+		title: title,
+		content: content,
+	}, {
+		headers: {
+			Authorization: token,
+		},
+	});
+	return response.data.data;
 };
 
 
 export const deleteTodo = async (id: string, token: string) => {
-    await axios.delete(`${API_ROUTES.TODO}/${id}`, {
-        headers: {
-            Authorization: token,
-        }
-    });
+	await axios.delete(`${API_ROUTES.TODO}/${id}`, {
+		headers: {
+			Authorization: token,
+		}
+	});
 };
