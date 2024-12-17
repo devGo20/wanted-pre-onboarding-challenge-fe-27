2ba49086819c80a2a6f5568efcdb644113297ae2
devGO <been2zz@naver.com>
2024-12-17 13:51:08 +0900

    feat: QueryParams type

--- src/model/api.ts
+++ src/model/api.ts
@@ -1,3 +1,11 @@
+export type QueryParams = {
+  countOnly: string;
+  keyword: string;
+  order: string;
+  priority: string;
+  sort: string;
+};
+
 export interface ApiError {
   response?: {
     data?: {
