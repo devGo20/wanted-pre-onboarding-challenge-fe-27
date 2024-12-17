314965c2657b0060e29cffaa91960724a1466766
devGO <been2zz@naver.com>
2024-12-17 15:35:17 +0900

    fix: TodoList > Todo 추가 관련 제거

--- src/pages/Todo/TodoList.tsx
+++ src/pages/Todo/TodoList.tsx
@@ -1,73 +1,14 @@
-import { useState } from 'react';
-import { checkValidation } from '../../util/todoHelper';
-import { Priority, Todo } from '../../model/todo';
+import { Todo } from '../../model/todo';
 
 interface TodoListProps {
   todos: Todo[];
   onSelectTodo: (todo: Todo) => void;
-  onAddTodo: (title: string, content: string, priority: string) => void;
 }
 
-const TodoList: React.FC<TodoListProps> = ({ todos, onSelectTodo, onAddTodo }) => {
-  const [isAdding, setIsAdding] = useState(false);
-  const [priority, setPriority] = useState<Priority>('normal');
-  const [title, setTitle] = useState<string>('');
-  const [content, setContent] = useState<string>('');
-
-  const handleSelectPriority = (selectedPriority: Priority) => {
-    setPriority(selectedPriority);
-  };
-
-  const handleAddTodo = () => {
-    if (checkValidation(title, content)) {
-      alert('할 일을 입력해주세요!');
-      return;
-    }
-    onAddTodo(title, content, priority);
-    resetAddData();
-  };
-
-  const resetAddData = () => {
-    setTitle('');
-    setContent('');
-    setIsAdding(false);
-  };
-
+const TodoList: React.FC<TodoListProps> = ({ todos, onSelectTodo }) => {
   return (
     <div>
       <h2>Todo List</h2>
-      {isAdding ? (
-        <div>
-          <div style={{ display: 'flex', gap: '20px' }}>
-            {Object.values(Priority).map((item) => (
-              <button
-                key={item}
-                onClick={() => handleSelectPriority(item)}
-                style={{
-                  border: 'none',
-                  borderBottom: priority === item ? '2px solid black' : '2px solid transparent',
-                }}
-              >
-                {item}
-              </button>
-            ))}
-          </div>
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <input
-            type="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
-          <button onClick={handleAddTodo}>저장</button>
-          <button onClick={() => resetAddData()}>취소</button>
-        </div>
-      ) : (
-        <button onClick={() => setIsAdding(true)}>추가</button>
-      )}
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
