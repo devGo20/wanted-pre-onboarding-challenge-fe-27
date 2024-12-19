4a40454dfe38a3e9cd5c908de6c818c1644c0d55
devGO <been2zz@naver.com>
2024-12-19 14:10:45 +0900

    feat: Option type 정의

---
+++ src/model/option.ts
@@ -0,0 +1,11 @@
+export interface Option<T = any> {
+  label: string;
+  value: T;
+}
+
+export const SortOptions: Option<{ sort: string; order: string; }>[] = [
+  { label: "최근 생성순", value: { sort: "createdAt", order: "desc" } },
+  { label: "오래된 생성순", value: { sort: "createdAt", order: "asc" } },
+  { label: "최근 수정순", value: { sort: "updatedAt", order: "desc" } },
+  { label: "오래된 수정순", value: { sort: "updatedAt", order: "asc" } },
+];
