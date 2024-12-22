3bb4fd930bbec00224dd722192829e82f2a05268
devGO <been2zz@naver.com>
2024-12-22 22:09:41 +0900

    fix: TodoForm > TodoPage, TodoDetail에서 쓸 수 있도록 컴포넌트 재정의

--- src/compontent/TodoForm.tsx
+++ src/compontent/TodoForm.tsx
@@ -1,61 +1,56 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { checkValidation } from "../util/todoHelper";
-import { useTodos } from "../queries/Todo";
-import { useNavigate } from "react-router-dom";
 import { PriorityButtons } from "./PriorityButtons";
 
-const TodoForm: React.FC = () => {
-  const [isAdding, setIsAdding] = useState(false);
-  const [priority, setPriority] = useState<string>('normal');
-  const [title, setTitle] = useState<string>('');
-  const [content, setContent] = useState<string>('');
-  const { addTodoMutation } = useTodos();
-  const navigate = useNavigate();
+interface TodoFormProps {
+  initialTitle?: string;
+  initialContent?: string;
+  initialPriority?: string;
+  onSubmit: (data: { title: string; content: string; priority: string; }) => void;
+  onCancel: () => void;
+}
 
-  const handleSelectPriority = (selectedPriority: string) => {
-    setPriority(selectedPriority);
-  };
+const TodoForm: React.FC<TodoFormProps> = ({
+  initialTitle = '',
+  initialContent = '',
+  initialPriority = 'normal',
+  onSubmit,
+  onCancel
+}) => {
+  const [title, setTitle] = useState(initialTitle);
+  const [content, setContent] = useState(initialContent);
+  const [priority, setPriority] = useState(initialPriority);
 
-  const handleSubmitTodo = () => {
+  const handleSubmit = () => {
     if (checkValidation(title, content)) {
       alert('할 일을 입력해주세요!');
       return;
     }
-    onAddTodo(title, content, priority);
-    resetAddData();
-  };
-  const onAddTodo = (title: string, content: string, priority: string) => {
-    addTodoMutation.mutate({ title, content, priority });
-    navigate('/');
-  };
-  const resetAddData = () => {
-    setTitle('');
-    setContent('');
-    setIsAdding(false);
+    onSubmit({ title, content, priority });
   };
 
-  return isAdding ? (
+  return (
     <div>
-      <PriorityButtons onSelect={handleSelectPriority} selectedPriority={priority} />
+      <PriorityButtons
+        onSelect={setPriority}
+        selectedPriority={priority}
+      />
       <input
         type="text"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
         placeholder="Title"
       />
-      <input
-        type="text"
+      <textarea
         value={content}
         onChange={(e) => setContent(e.target.value)}
         placeholder="Content"
       />
       <div>
-        <button onClick={handleSubmitTodo}>저장</button>
-        <button onClick={() => resetAddData()}>취소</button>
+        <button onClick={handleSubmit}>저장</button>
+        <button onClick={onCancel}>취소</button>
       </div>
     </div>
-  ) : (
-    <button onClick={() => setIsAdding(true)}>추가</button>
   );
 };
 
