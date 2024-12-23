e142423ebaa6a49ba8cc0715d683cb58eb494242
devGO <been2zz@naver.com>
2024-12-23 17:14:37 +0900

    fix: style -> tailwind class로 변경

--- src/compontent/PriorityButtons.tsx
+++ src/compontent/PriorityButtons.tsx
@@ -6,15 +6,15 @@ interface PriorityButtonsProps {
 }
 export const PriorityButtons: React.FC<PriorityButtonsProps> = ({ selectedPriority, onSelect }) => {
   return (
-    <div style={{ display: 'flex', gap: '20px' }}>
+    <div className="flex">
       {Object.values(Priority).map((item) => (
         <button
           key={item}
           onClick={() => onSelect(item)}
-          style={{
-            border: 'none',
-            borderBottom: selectedPriority === item ? '2px solid black' : '2px solid transparent',
-          }}
+          className={`px-4 py-2 rounded ${selectedPriority === item
+            ? 'bg-black text-white'
+            : 'bg-gray-200 text-black hover:bg-gray-300'
+            }`}
         >
           {item}
         </button>
