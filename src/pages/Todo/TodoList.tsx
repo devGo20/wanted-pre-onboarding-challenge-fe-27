fd5aac45351852957ed1a155072bf4f79f636243
devGO <been2zz@naver.com>
2024-12-27 22:38:13 +0900

    feat: TodoList > Todo 체크 박스 추가

--- src/pages/Todo/TodoList.tsx
+++ src/pages/Todo/TodoList.tsx
@@ -1,4 +1,5 @@
 import { Todo } from '../../model/todo';
+import { useTodos } from '../../queries/Todo';
 
 interface TodoListProps {
   todos: Todo[];
@@ -6,6 +7,11 @@ interface TodoListProps {
 }
 
 const TodoList: React.FC<TodoListProps> = ({ todos, onSelectTodo }) => {
+  const { updateCompleteTodoMutation } = useTodos();
+  const handleCheckboxClick = async (id: string, e: React.MouseEvent) => {
+    e.stopPropagation();
+    await updateCompleteTodoMutation.mutateAsync(id);
+  };
   return (
     <div>
       <h2>Todo List</h2>
@@ -16,8 +22,13 @@ const TodoList: React.FC<TodoListProps> = ({ todos, onSelectTodo }) => {
             onClick={() => onSelectTodo(todo)}
             className="flex items-center h-12 px-8 border border-gray rounded-lg cursor-pointer"
           >
-            <h3>{todo.title}</h3>
-            {/* <p>{todo.content}</p> */}
+            <input
+              onClick={(event) => handleCheckboxClick(todo.id, event)}
+              checked={todo.complete}
+              type="checkbox"
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <h3 className={`text-lg ${todo.complete ? 'line-through text-gray-500' : ''}`}>{todo.title}</h3>
           </li>
         ))}
       </ul>
