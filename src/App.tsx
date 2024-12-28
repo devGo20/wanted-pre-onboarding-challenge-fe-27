28682e15582ab6456997e7eb2235db22754ef194
devGO <been2zz@naver.com>
2024-12-28 23:01:11 +0900

    feat: Todo PrivateRoute 설정

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import TodoPage from './pages/Todo/TodoPage';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import PrivateRouter from './compontent/PrivateRouter';
+import NotFound from './pages/NotFound';
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -15,23 +17,30 @@ export const queryClient = new QueryClient({
     },
   },
 });
+
 function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
         <Routes>
-          <Route path="/" element={<Navigate to="/todos" replace />} />
-          <Route path="/auth/login" element={<Login />} />
-          <Route path="/auth/signup" element={<Signup />} />
-          <Route path="/todos/*" element={<TodoPage />} />
+          <Route path="/auth">
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+          </Route>
+
+          <Route element={<PrivateRouter />}>
+            <Route path="/" element={<Navigate to="/todos" replace />} />
+            <Route path="/todos/*" element={<TodoPage />} />
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
         </Routes>
-        <ToastContainer
-          position="bottom-center"
-          hideProgressBar={false} />
+        <ToastContainer position="bottom-center" hideProgressBar={false} />
       </QueryClientProvider>
     </>
   );
 }
+
 
 export default App;
